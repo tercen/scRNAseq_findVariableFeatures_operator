247c6a86111f6e92a625602bdeb759aaf24bf48f
Filipe Tavares-Cadete <filipe@tavares.cadete.eu>
2020-08-28 23:04:18 +0000

    Means, variance and trend calculated.

--- workspace.R
+++ workspace.R
@@ -1,15 +1,26 @@
-library(tercen)
+library(scRNAseq)
+library(SingleCellExperiment)
+library(scran)
+library(tidyr)
 library(dplyr)
+library(tercen)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "7d6077b7fa4df6315a718714de00346e")
+options("tercen.stepId"     = "e8df35d9-2f50-45e0-a192-671c49c34418")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
+(ctx = tercenCtx())
+
+count_matrix <- ctx$as.matrix()
+
+dec <- trendVar(count_matrix)
+
+output <- tibble(.ri = 0:(nrow(count_matrix) - 1),
+                 mean_expression = dec$means,
+                 variance = dec$vars,
+                 trend = dec$trend(dec$means))
+
+ctx$addNamespace(output) %>%
   ctx$save()
